057570a5d30c8128f5f8fd0f024581683706de7e
Wilton Oliveira <oliveirajrwilton@gmail.com>
2018-10-16 23:34:22 -0300

    implementando disparo e seus movimentos

--- battle.c
+++ battle.c
@@ -4,6 +4,7 @@
 #define TAM_LIN 20
 #define TAM_COL 60
 
+
 struct Robo {
 	float life;
 	int ammo;
@@ -14,18 +15,36 @@ struct Robo {
 };
 typedef struct Robo robos;
 
+struct Disparo {
+	int lin, col;
+	int direcao;
+};
+typedef struct Disparo disparo;
+
 void inicia_robo (robos* robo);
 void arena (robos* robo);
 void imprimir_robo ();
+void imprimir_disparo ();
 
 void move_up (robos* robo);
 void move_down (robos* robo);
 void move_left (robos* robo);
 void move_right (robos* robo);
+
 void diag_sup_esq (robos* robo);
 void diag_sup_dir (robos* robo);
 void diag_inf_esq (robos* robo);
 void diag_inf_dir (robos* robo);
+
+void dis_up (disparo* dis);
+void dis_down (disparo* dis);
+void dis_left (disparo* dis);
+void dis_right (disparo* dis);
+
+void dis_sup_e (disparo* dis);
+void dis_sup_d (disparo* dis);
+void dis_inf_e (disparo* dis);
+void dis_inf_d (disparo* dis);
 
 int main()
 {	
@@ -44,6 +63,33 @@ void inicia_robo (robos* robo){
 	
 	robo->lin = 4;
 	robo->col = 2;
+}
+
+void arena (robos* robo){
+	int i, j;
+
+	for (i = 0; i <= TAM_LIN; i++){
+		for (j = 0; j <= TAM_COL; j++)
+			if (i == 0 || i == TAM_LIN)
+				printf ("_");
+				
+			else if (j == 0 || j == TAM_COL)
+				printf ("|");
+			
+			else if (i == robo->lin && j == robo->col)
+						imprimir_robo ();
+		
+			
+			else
+				printf (" ");
+				
+			
+		printf ("\n");
+	}
+	system ("PAUSE");
+	diag_inf_dir (robo);
+	arena (robo);
+	
 }
 
 void move_up (robos* robo){
@@ -80,32 +126,48 @@ void diag_inf_dir (robos* robo){
 	move_right (robo);
 }
 
-void arena (robos* robo){
-	int i, j;
-
-	for (i = 0; i <= TAM_LIN; i++){
-		for (j = 0; j <= TAM_COL; j++)
-			if (i == 0 || i == TAM_LIN)
-				printf ("_");
-				
-			else if (j == 0 || j == TAM_COL)
-				printf ("|");
-			
-			else if (i == robo->lin && j == robo->col)
-						imprimir_robo ();
-		
-			
-			else
-				printf (" ");
-				
-			
-		printf ("\n");
-	}
-	system ("PAUSE");
-	diag_inf_dir (robo);
-	arena (robo);
-	
+void dis_up (disparo* dis){
+	dis->lin = dis->lin - 1;
 }
+
+void dis_down (disparo* dis){
+	dis->lin = dis->lin + 1;
+}
+
+void dis_left (disparo* dis){
+	dis->col = dis->col - 1;
+}
+
+void dis_right (disparo* dis){
+	dis->col = dis->col + 1;
+}
+
+void dis_sup_e (disparo* dis){
+	dis_up (dis);
+	dis_left (dis);
+}
+
+void dis_sup_d (disparo* dis){
+	dis_up (dis);
+	dis_right (dis);
+}
+
+void dis_inf_e (disparo* dis){
+	dis_down (dis);
+	dis_left (dis);
+}
+
+void dis_inf_d (disparo* dis){
+	dis_down (dis);
+	dis_right (dis);
+}
+
+
+
+void imprimir_disparo (){
+	printf (".");
+}
+
 
 void imprimir_robo (){	
 	printf ("%c", 219);
